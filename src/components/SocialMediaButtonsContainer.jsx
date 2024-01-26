0d8dffa1a512a395b4f595c666d564843b226aa4
vishal kumar shaw <vishalkumar.msit@gmail.com>
2024-01-26 14:15:38 +0530

    Update social links

--- src/components/SocialMediaButtonsContainer.jsx
+++ src/components/SocialMediaButtonsContainer.jsx
@@ -5,7 +5,7 @@ function SocialMediaButtonsContainer() {
   return (
     <div className="social-media">
       <SocialMediaButton
-        address="https://example.com/github"
+        address="https://github.com/darkradiation/Pokemon"
         icon="https://cdn4.iconfinder.com/data/icons/social-media-2231/512/71-github_social-64.png"
       >
         GitHub
@@ -23,7 +23,7 @@ function SocialMediaButtonsContainer() {
         Instagram
       </SocialMediaButton>
       <SocialMediaButton
-        address="https://example.com/linkedin"
+        address="https://www.linkedin.com/in/vishal-kumar-shaw-8a037024a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
         icon="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Linkedin2_svg-128.png"
       >
         LinkedIn
@@ -48,13 +48,13 @@ function SocialMediaButtonsContainer() {
         Reddit
       </SocialMediaButton> */}
       <SocialMediaButton
-        address="https://example.com/whatsapp"
+        address="https://wa.me/qr/PM3GDPTJLE7XD1"
         icon="https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Whatsapp_svg-128.png"
       >
         WhatsApp
       </SocialMediaButton>
       <SocialMediaButton
-        address="https://example.com/telegram"
+        address=""
         icon="https://cdn2.iconfinder.com/data/icons/social-media-solid-2/32/Telegram-64.png"
       >
         Telegram
